d951d8cdb8076d1ac449214a80af15ee67dd25a3
benmalik <benjamin.malik@gmail.com>
2015-01-09 18:40:45 +0100

    changed fonts and style of buttons

--- BikeTec/obj/Debug/BikeTec_Content.g.i.cs
+++ BikeTec/obj/Debug/BikeTec_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/bikenride.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/bikenridesmall.png")]
 
 
